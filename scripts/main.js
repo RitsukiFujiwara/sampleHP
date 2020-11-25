e2c6d3c193e50ab65c437790f553a6eef72f4552
fujiwara ritsuki <>
2020-11-25 20:34:03 +0900

    no message

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,11 @@ document.addEventListener('DOMContentLoaded', function () {
     hero.start();
 });
 
+const cb = function (el, isIntersecting) {
+    if(isIntersecting) {
+        const ta = new TextAnimation(el);
+        ta.animate();
+    }
+}
+
+const so = new ScrollObserver('.animate-title', cb);
