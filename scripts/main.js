ab3deac83b5bde1cc88211b204befdcb6228c90f
fujiwara ritsuki <>
2020-11-26 20:39:26 +0900

    no message

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const hero = new HeroSlider('.swiper-container');
     hero.start();
-});
+
 
 const cb = function (el, isIntersecting) {
     if(isIntersecting) {
@@ -11,3 +11,10 @@ const cb = function (el, isIntersecting) {
 }
 
 const so = new ScrollObserver('.animate-title', cb);
+
+const _inviewAnimation = function(el, ){
+
+}
+
+const so = new ScrollObserver('.tween-animate-title', _inviewAnimation);
+});
